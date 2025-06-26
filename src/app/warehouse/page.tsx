0da9af7036fc571ad4282c528394fab1cd3cc2d0
patna <p.wiriyar@gmail.com>
2025-06-26 13:28:09 +0700

    picker meter form

---
+++ src/app/warehouse/page.tsx
@@ -0,0 +1,10 @@
+import MeterForm from "@/app/warehouse/meter-form";
+
+export default function WarehousePage() {
+  return (
+    <div className="max-w-xl mx-auto mt-10">
+      <h1 className="text-xl font-bold mb-4">เบิกมิเตอร์ใหม่</h1>
+      <MeterForm />
+    </div>
+  )
+}
---
+++ src/shema/meter-form.ts
@@ -0,0 +1,10 @@
+import z from "zod"
+
+export const meterFormSchema = z.object({
+  peaNoNew: z.string().min(1, 'กรุณากรอกหมายเลข PEA No.'),
+  issuerName: z.string(),
+  issueDate: z.date(), // เปลี่ยนจาก string เป็น date
+  newMeterPhoto: z.instanceof(File),
+})
+
+export type MeterFormData = z.infer<typeof meterFormSchema>
